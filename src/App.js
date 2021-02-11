21b95d522c514d037ebe11267142cd49ac43dea2
ahmedseragcodes <ahmedserag1@gmail.com>
2021-02-11 15:47:07 -0500

    completed and passed tests for App file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ContactForm from "./components/ContactForm";
 export default function App() {
   return (
     <div className="App">
+      <h1>Contact Form</h1>
       <ContactForm />
     </div>
   );
--- src/testing/App.test.js
+++ src/testing/App.test.js
@@ -2,3 +2,12 @@ import React from "react";
 import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
+
+
+test("App component renders without error", ()=>{
+    render(<App />);
+
+    const headingContainer=screen.getByText(/Contact Form/i);
+
+    expect(headingContainer).toBeInTheDocument();
+})
